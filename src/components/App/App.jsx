f53368a231e1f4632260b95ad44680a1c572292b
Volodymyr Klochan <vovkis13@gmail.com>
2021-12-09 21:51:43 +0200

    addContact refactored

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,33 +5,33 @@ import ContactForm from '../ContactForm';
 import Filter from '../Filter';
 import ContactList from '../ContactList/';
 
-function filterContacts(contacts, filter) {
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(filter.toLowerCase()),
-  );
-}
-
 export default function App() {
   const [contacts, setContacts] = useState(
     () => JSON.parse(localStorage.getItem('contacts')),
     [],
   );
   const [filter, setFilter] = useState('');
-
-  const filteredContacts = filterContacts(contacts, filter);
+  const [filteredContacts, setFilteredContacts] = useState(() => contacts, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
     setFilter('');
   }, [contacts]);
 
+  useEffect(() => {
+    setFilteredContacts(
+      contacts.filter(({ name }) =>
+        name.toLowerCase().includes(filter.toLowerCase()),
+      ),
+    );
+  }, [filter, contacts]);
+
   const addItemToContacts = (e, newName, newNumber) => {
     e.preventDefault();
-    if (
-      contacts.find(({ name }) => name.toLowerCase() === newName.toLowerCase())
-    )
-      return window.alert(`${newName} is already in contacts.`);
-
+    const found = contacts.find(
+      ({ name }) => name.toLowerCase() === newName.toLowerCase(),
+    );
+    if (found) return window.alert(`${newName} is already in contacts.`);
     setContacts(prevState => [
       { id: nanoid(), name: newName, number: newNumber },
       ...prevState,
